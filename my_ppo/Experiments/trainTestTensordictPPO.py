360f387ca87e85a91c17793af561008825b5b424
m2430126 <y.matsuda@uec.ac.jp>
2025-01-11 23:50:38 +0900

    same as precommit

--- my_ppo/Experiments/trainTestTensordictPPO.py
+++ my_ppo/Experiments/trainTestTensordictPPO.py
@@ -10,3 +10,19 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+
+env = gym.make("CartPole-v1")
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
